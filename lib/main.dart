3f971b0381bc65ca94926e2ca69eba1d546efff6
nori <noritaro12@gmail.com>
2020-11-12 16:25:42 +0900

    update parts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import './pages/list.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: ListPage(),
+        home: ParentWidget(),
         title: 'provider examples',
         theme: ThemeData(
           textTheme: const TextTheme(
@@ -12,3 +14,67 @@ void main() => runApp(
         ),
       ),
     );
+
+class HomeWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChatApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+//      home: MyAuthPage(),
+//      home: MyFirestorePage(),
+      home: ParentWidget(),
+    );
+  }
+}
+
+
+class CountData extends ChangeNotifier{
+  int count =0;
+
+  void increment(){
+    count +=1;
+
+    notifyListeners();
+  }
+}
+
+class ParentWidget extends StatelessWidget{
+
+  final data = CountData();
+  @override
+  Widget build(BuildContext context) {
+
+      return Provider<CountData>.value(
+        value: data,
+        child: Container(
+          child: Childwidget(),
+        ),
+      );
+
+  }
+}
+
+class Childwidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final CountData data = Provider.of<CountData>(context);
+    return Column(
+      children: [
+        Text('count is ${data.count.toString()}'),
+        RaisedButton(
+          child: Text("Count up"),
+          onPressed: (){
+            data.increment();
+          },
+        )
+      ],
+    );
+
+  }
+}
+
