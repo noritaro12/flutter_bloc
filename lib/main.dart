9fe7faa96257f65c9eee6347a010f4f61f0b07b0
nori <noritaro12@gmail.com>
2020-11-13 18:23:48 +0900

    update business logic controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,107 @@
-// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: ParentWidget(),
-        title: 'provider examples',
-        theme: ThemeData(
-          textTheme: const TextTheme(
-            bodyText2: TextStyle(fontSize: 48.0),
-          ),
-        ),
-      ),
-    );
 
-class HomeWidget extends StatelessWidget{
+class CounterBloc {
+  final _actionController = StreamController<void>();
+  Sink<void> get sink {
+    return _actionController.sink;
+  }
+
+  final _countController = StreamController<int>();
+  Stream<int> get stream {
+    return _countController.stream;
+  }
+
+  int _count = 0;
+
+  CounterBloc() {
+    _actionController.stream.listen((_) {
+      _count++;
+      _countController.sink.add(_count);
+    });
+  }
+
+  void dispose() {
+    _actionController.close();
+    _countController.close();
+  }
+}
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ChatApp',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-//      home: MyAuthPage(),
-//      home: MyFirestorePage(),
-      home: ParentWidget(),
+      home: Provider<CounterBloc>(
+        create: (context)=>CounterBloc(),
+        dispose: (context, bloc)=>bloc.dispose(),
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
+      )//,
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
 
-class CountData extends ChangeNotifier{
-  int count =0;
-
-  void increment(){
-    count +=1;
-
-    notifyListeners();
-  }
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class ParentWidget extends StatelessWidget{
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  final data = CountData();
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
-      return Provider<CountData>.value(
-        value: data,
-        child: Container(
-          child: Childwidget(),
+    final counterBloc = Provider.of<CounterBloc>(context);
+     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<int>(
+              stream: counterBloc.stream,
+              builder: (context, snapshot) {
+                return Text(
+                  '${snapshot?.data ?? 100}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              }
+            ),
+          ],
         ),
-      );
-
-  }
-}
-
-class Childwidget extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    final CountData data = Provider.of<CountData>(context);
-    return Column(
-      children: [
-        Text('count is ${data.count.toString()}'),
-        RaisedButton(
-          child: Text("Count up"),
-          onPressed: (){
-            data.increment();
-          },
-        )
-      ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:()=> counterBloc.sink.add(null),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-
   }
 }
+
+
+
 
